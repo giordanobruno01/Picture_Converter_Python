c586c07db1158d9bdf38affd5294c961b4e19fce
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-18 19:00:07 +0100

    extract the extension and add to menu

--- picConverter.py
+++ picConverter.py
@@ -2,15 +2,23 @@ from PIL import Image
 class picConverter:
 
     def converter(self, path):
-        # img = Image.open(path)
+        img = Image.open(path)
         stop =0
         for i in range(len(path)):
           if(path[i]=="."):
             stop = i
-        print(path[stop:])
+        ext = path[stop:].lower()
+        print("The current extension is: ", ext)
+        arr = [".jpg", ".jpeg", ".png"]
+        
+        arr.remove(ext)
+        
+        for i in range(len(arr)):
+          print(i+1," for ",arr[i])
+        choice = int(input())
+        
 
 path = input("paste the image path here: ")
 
 obj = picConverter()
 obj.converter(path)
-
