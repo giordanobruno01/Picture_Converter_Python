f6b6dbea1274b864be10c04e2f4be82df9ab176e
Giordano Bruno <giordanobrunotj@hotmail.com>
2023-07-18 19:28:50 +0100

    converter running ok

--- picConverter.py
+++ picConverter.py
@@ -1,7 +1,9 @@
 from PIL import Image
+import os
 class picConverter:
 
     def converter(self, path):
+        pathCopy = path
         img = Image.open(path)
         stop =0
         for i in range(len(path)):
@@ -16,9 +18,17 @@ class picConverter:
         for i in range(len(arr)):
           print(i+1," for ",arr[i])
         choice = int(input())
+        p = path[:stop]
+        path = p+ arr[choice-1]
         
+        print(path)
+        
+        img.save(path)
+        os.remove(pathCopy)
 
 path = input("paste the image path here: ")
 
 obj = picConverter()
 obj.converter(path)
+
+
